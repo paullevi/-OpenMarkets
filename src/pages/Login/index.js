f40d3af1bf39d4f7057369bf9e53f8840b4aac8f
paullevi <p.levi.freelancer@gmail.com>
2021-06-02 00:59:57 +1000

    refactor: fix the issues

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,206 +1,208 @@
-import React, { useState } from 'react';
-import { Formik } from 'formik';
-import { Link } from 'react-router-dom';
-import { makeStyles } from '@material-ui/core/styles';
-import * as Yup from 'yup';
-import clsx from  'clsx';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Divider from '@material-ui/core/Divider';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import LockIcon from '@material-ui/icons/Lock';
-import PersonIcon from '@material-ui/icons/Person';
-import Typography from '@material-ui/core/Typography';
-import Visibility from '@material-ui/icons/Visibility';
-import VisibilityOff from '@material-ui/icons/VisibilityOff';
-import { ReactComponent as GoogleIcon } from '../../icon/google.svg'
-import TextField from '../../components/TextField';
-import Button from '../../components/Button';
+import React, { useState } from "react";
+import { Formik } from "formik";
+import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import * as Yup from "yup";
+import clsx from  "clsx";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Divider from "@material-ui/core/Divider";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import LockIcon from "@material-ui/icons/Lock";
+import PersonIcon from "@material-ui/icons/Person";
+import Typography from "@material-ui/core/Typography";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import { ReactComponent as GoogleIcon } from "../../icon/google.svg"
+import TextField from "../../components/TextField";
+import Button from "../../components/Button";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: theme.palette.common.white,
-    display:'flex',
-    '@media (max-width: 1024px)': {
-      display:'block'
+    display: "flex",
+    "@media (max-width: 1024px)": {
+      display: "block"
     }
   },
   signup: {
     marginTop: 50,
     fontSize: 15,
-    fontWeight: 500,
-    textDecoration:'underline',
-    color: '#333333',
-    '&:hover': {
-      color:'#27C7FE'
+    fontWeight: 600,
+    textDecoration: "underline",
+    color: "#333333",
+    "&:hover": {
+      color: "#27C7FE"
     },
   },
   forgotButton: {
     fontSize: 16,
-    lineHeight: '20px',
+    lineHeight: "20px",
     color: theme.palette.text.primary,
     marginBottom: 33,
-    display: 'block',
-    fontFamily: 'Source Sans Pro',
-    cursor: 'pointer',
-    textAlign:'right'
+    display: "block",
+    fontFamily: "Work Sans",
+    fontWeight: "600",
+    cursor: "pointer",
+    textAlign: "right",
+    textDecoration: "underline",
   },
   errorLabel: {
     marginBottom: 15,
-    textAlign: 'center',
+    textAlign: "center",
   },
   logoImg:{
-    width:'65%',
-    minWidth:'30%'
+    width: "65%",
+    minWidth: "30%"
   },
   bgImageWrapper:{
     zIndex: 0,
-    position: 'absolute',
-    height: '100vh'
+    position: "absolute",
+    height: "100vh"
   },
   bgImage: {
-    width:'100%',
-    height:'100vh',
-    '@media (max-width: 1024px)': {
+    width: "100%",
+    height: "100vh",
+    "@media (max-width: 1024px)": {
       fontSize:28,
-      display:'none'
+      display: "none"
     }
   },
   loginForm:{
-    position: 'relative',
-    width: '30%',
-    padding:'90px 60px',
-    display: 'inline-block',
-    '@media (max-width: 1024px)': {
-      width: '100%',
-      padding:'120px 50px 80px',
+    position: "relative",
+    width: "35%",
+    padding: "90px 60px",
+    display: "inline-block",
+    "@media (max-width: 1024px)": {
+      width: "100%",
+      padding: "120px 50px 80px",
       zIndex:999
     },
-    '@media (max-width: 568px)': {
-      width: '100%',
-      padding:'120px 40px 50px',
+    "@media (max-width: 568px)": {
+      width: "100%",
+      padding: "120px 40px 50px",
       zIndex:999
     },
   },
   description:{
-    position: 'relative',
-    width:'70%',
-    '@media (max-width: 1024px)': {
-      width: '100%',
+    position: "relative",
+    width: "65%",
+    "@media (max-width: 1024px)": {
+      width: "100%",
       zIndex:0
     }
   },
   bgContent:{
-    textAlign:'left',
-    '@media (max-width: 1024px)': {
-      display:'none'
+    textAlign: "left",
+    "@media (max-width: 1024px)": {
+      display: "none"
     }
   },
   cardLeftRoot:{
-    maxWidth:'100%',
+    maxWidth: "100%",
     borderRadius:8,
-    border: '1px solid #c3b2d4',
-    '@media (max-width: 1024px)': {
-      width:'100%',
-      display:'block'
+    border: "1px solid #c3b2d4",
+    "@media (max-width: 1024px)": {
+      width: "100%",
+      display: "block"
     }
   },
   cardLeftRootInactive:{
-    maxWidth:'100%',
+    maxWidth: "100%",
     borderRadius:8,
-    border: '1px solid #c3b2d4',
-    '@media (max-width: 1024px)': {
-      display:'none',
+    border: "1px solid #c3b2d4",
+    "@media (max-width: 1024px)": {
+      display: "none",
     }
   },
   cardRightRoot:{
-    maxWidth:'100%',
+    maxWidth: "100%",
     borderRadius:8,
-    border: '1px solid #c3b2d4',
-    '@media (max-width: 1024px)': {
-      display:'none'
+    border: "1px solid #c3b2d4",
+    "@media (max-width: 1024px)": {
+      display: "none"
     }
   },
   cardRightRootActive:{
-    maxWidth:'100%',
+    maxWidth: "100%",
     borderRadius:8,
-    border: '1px solid #c3b2d4',
-    '@media (max-width: 1024px)': {
-      display:'block'
+    border: "1px solid #c3b2d4",
+    "@media (max-width: 1024px)": {
+      display: "block"
     }
   },
   cardGroup:{
-    display:'flex',
-    marginTop:'15%',
-    justifyContent:'space-between',
-    '@media (max-width: 1024px)': {
-      width:'100%',
-      display:'block',
+    display: "flex",
+    marginTop: "15%",
+    justifyContent: "space-between",
+    "@media (max-width: 1024px)": {
+      width: "100%",
+      display: "block",
       marginTop:0
     }
   },
   card:{
-    width:'50%',
-    marginRight:'3%',
-    borderRadius:'1px soild black',
-    '@media (max-width: 1024px)': {
-      width:'100%',
+    width: "45%",
+    borderRadius: "1px soild black",
+    "@media (max-width: 1024px)": {
+      width: "100%",
       margin:0
     }
   },
 
   bgDescription:{
     zIndex: 1,
-    position:'relative',
-    padding:'120px 70px 10px',
-    color:'white',
-    '@media (max-width: 1024px)': {
-      padding:'0px 40px'
+    position: "relative",
+    padding: "120px 70px 10px",
+    color: "white",
+    "@media (max-width: 1024px)": {
+      padding: "0px 40px"
     },
   },
   bottomRightLabel:{
     marginTop:6,
-    color:'#b8c0de',
-    textAlign:'right',
+    color: "#b8c0de",
+    textAlign: "right",
   },
   bottomLeftLabel:{
     marginTop:6,
-    color:'#b8c0de',
-    textAlign:'right',
-    '@media (max-width: 1024px)': {
-      display:'none'
+    color: "#b8c0de",
+    textAlign: "right",
+    "@media (max-width: 1024px)": {
+      display: "none"
     },
   },
   stepper:{
-    display:'none',
-    '@media (max-width: 1024px)': {
-      display:'block',
-      position: 'relative', 
-      width: '100%', 
+    display: "none",
+    "@media (max-width: 1024px)": {
+      display: "block",
+      position: "relative", 
+      width: "100%", 
       bottom:36
     },
   },
   sliderLabel:{
-    textAlign:'center',
+    textAlign: "center",
     fontSize: 13,
-    backgroundColor:'#FFFFFF',
-    display: 'block',
+    backgroundColor: "#FFFFFF",
+    display: "block",
     height: 11,
     width: 11,
-    borderRadius: '50%',
-    color: 'white',
-    border:'1px solid #9d9d9d'
+    borderRadius: "50%",
+    color: "white",
+    border: "1px solid #9d9d9d"
   },
   sideBarItem:{
-    display:'inline-block',
+    display: "inline-block",
     // marginLeft:14
   },
   inlineLink:{
-    color: '#3D9CC1',
-    '&:hover': {
-      color:'#27C7FE',
-      textDecoration:'underline'
+    color: "#3D9CC1",
+    textDecoration: "none",
+    "&:hover": {
+      color: "#27C7FE",
+      textDecoration: "underline"
     },
   },
   media:{
@@ -211,14 +213,14 @@ const useStyles = makeStyles((theme) => ({
     width:80,
   },
   chageColor: {
-    backgroundColor:'#9d9d9d',
+    backgroundColor: "#9d9d9d",
   },
 
 }));
 
 const schema = Yup.object().shape({
-  email: Yup.string().email('Invalid email. Please try again').required('Email is required'),
-  password: Yup.string().min(6, 'Minimum 6 characters required!').required('Password is required'),
+  email: Yup.string().email("Invalid email. Please try again").required("Email is required"),
+  password: Yup.string().min(6, "Minimum 6 characters required!").required("Password is required"),
 });
 
 export default function CPLoinForm({
@@ -235,9 +237,9 @@ export default function CPLoinForm({
   return (
     <div className={classes.root}>
       <div className={classes.loginForm}>
-        <img className={classes.logoImg} alt="logo" src='https://s3.amazonaws.com/om-exchange-production/assets/images/om_logo.png'/>
+        <img className={classes.logoImg} alt="logo" src="https://s3.amazonaws.com/om-exchange-production/assets/images/om_logo.png"/>
         <Formik
-          initialValues={{ email: '', password: '' }}
+          initialValues={{ email: "", password: "" }}
           validationSchema={schema}
         >
           {({
@@ -249,7 +251,7 @@ export default function CPLoinForm({
             isSubmitting,
             /* and other goodies */
           }) => (
-            <form style={{marginTop:60}}>
+            <form style={{ marginTop:60 }}>
               <TextField
                 id="email"
                 placeholder="Email address"
@@ -268,11 +270,11 @@ export default function CPLoinForm({
               />
               <TextField
                 id="password"
-                type={showPassword ? 'text' : 'password'}
+                type={showPassword ? "text" : "password"}
                 value={values.password}
                 placeholder="Password"
                 fullWidth
-                onChange={handleChange('password')}
+                onChange={handleChange("password")}
                 onBlur={handleBlur}
                 startAdornment={
                   <InputAdornment position="start">
@@ -295,7 +297,7 @@ export default function CPLoinForm({
               />
               <span
                 className={classes.forgotButton}
-                onClick={() => window.Intercom('showNewMessage', 'I forgot password and needs to replace it')}
+                onClick={() => window.Intercom("showNewMessage", "I forgot password and needs to replace it")}
               >Forgot password?</span>
               <Button
                 type="submit"
@@ -310,7 +312,7 @@ export default function CPLoinForm({
                 Log in
               </Button>
               <Button
-                style={{marginTop:20, border:'1px solid #9d9d9d'}}
+                style={{ marginTop:20, border: "1px solid #9d9d9d" }}
                 type="submit"
                 disabled={isSubmitting}
                 fullWidth
@@ -330,7 +332,7 @@ export default function CPLoinForm({
           )}
         </Formik>
         <Typography
-          style={{marginTop:50}}
+          style={{ marginTop:50 }}
           variant="body2"
           align="center"
         >
@@ -341,71 +343,79 @@ export default function CPLoinForm({
       </div>
       <div className={classes.description}>
         <div className={classes.bgImageWrapper}>
-          <img className={classes.bgImage} alt="Background" src='https://om-exchange-production.s3.amazonaws.com/assets/images/background-microscope.jpg'/>
+          <img className={classes.bgImage} alt="Background" src="https://om-exchange-production.s3.amazonaws.com/assets/images/background-microscope.jpg"/>
         </div>
         <div className={classes.bgDescription}>
           <div className={classes.bgContent}>
             <Typography variant="body1">OpenMarkets</Typography>
-            <Typography variant="h1" style={{marginTop:12}}>Simplifying Healthcare<br/>Equipment Purchasing</Typography>
-            <Typography variant="body1" style={{marginTop:14}}>OpenMarkets is the leading online platform for buying, selling<br/> 
-                and managing health care equipment, used by hundreds of <br/>
-                hospitals and equipment suppliers to facilitate transactions <br/>
-                and communications in the $80BN health care capital market.
+            <Typography variant="h1" style={{ marginTop:12 }}>
+              Simplifying Healthcare<br/>
+              Equipment Purchasing
+            </Typography>
+            <Typography variant="body1" style={{ marginTop:14 }}>
+              OpenMarkets is the leading online platform for buying, selling<br/> 
+              and managing health care equipment, used by hundreds of <br/>
+              hospitals and equipment suppliers to facilitate transactions <br/>
+              and communications in the $80BN health care capital market.
             </Typography>
           </div>
           <div className={classes.cardGroup}>
             <div className={classes.card}>
-              <Card className={toggleSelect? classes.cardLeftRoot:classes.cardLeftRootInactive}>
-                <CardContent style={{padding:2}}>
-                  <Typography variant="h2">
-                    Lizard
+              <Card className={toggleSelect? classes.cardLeftRoot : classes.cardLeftRootInactive}>
+                <CardContent style={{ padding:2 }}>
+                  <Typography variant="h3" style={{ padding: "4px 0" }}>
+                    EQUIPPED
                   </Typography>
                   <Divider/>
-                  <div style={{display:'flex', textAlign:'left'}}>
+                  <div style={{ display: "flex", textAlign: "left" }}>
                     <img className = {classes.adImg} src="https://www.openmarketshealth.com/uploads/logos/suppliers/cropped-kenmed-medium-1-1024x413.png" alt="cardLeftImage"/>
                     <Divider orientation="vertical" flexItem/>
-                    <Typography variant="subtitle2" style={{padding:'8px 10px'}}>
-                      Lizards are a widespread group of squamate reptiles, with over 6,000 species.
-                      <Typography>
+                    <div style={{ padding: "8px 10px" }}>
+                      <Typography variant="subtitle2">
+                        Medical equipments to outfit your Clinic, Surgery Center or Hospital
+                      </Typography>
+                      <Typography style={{ marginTop: "8px" }}>
                         <Link to ="#" className={classes.inlineLink}>
                           Connect With Equipped MD Here
                         </Link>
                       </Typography>
-                    </Typography>
+                    </div>
                   </div>
                 </CardContent>
               </Card>
-              <h5 className={classes.bottomLeftLabel}>promotion</h5>
+              <Typography variant="h5" className={classes.bottomLeftLabel}>promotion</Typography>
             </div>
             <div className={classes.card}>
               <Card className={toggleSelect? classes.cardRightRoot : classes.cardRightRootActive}>
-                <CardContent style={{padding:2}}>
-                  <Typography variant="h2">
-                    Lizard
+                <CardContent style={{ padding:2 }}>
+                  <Typography variant="h3" style={{ padding: "4px 0" }}>
+                    AIDEN
                   </Typography>
                   <Divider/>
-                  <div style={{display:'flex', textAlign:'left'}}>
+                  <div style={{ display: "flex", textAlign: "left" }}>
                     <img className = {classes.adImg} src="https://www.openmarketshealth.com/uploads/posts/cover-images/surgicaldirectlogo.jpg" alt="cardRightImage"/>
                     <Divider orientation="vertical" flexItem/>
-                    <Typography variant="subtitle2" style={{padding:'8px 10px'}}>
-                      Lizards are a widespread group of squamate reptiles, with over 6,000 species.
-                      <Typography>
+                    <div style={{ padding: "8px 10px" }}>
+                      <Typography variant="subtitle2">
+                        Aiden delivers vetted medical supplies to keep our frontlines safe.
+                      </Typography>
+                      <Typography style={{ marginTop: "8px" }}>
                         <Link to ="#" className={classes.inlineLink}>
                           Order N95s, Nitrile Gloves, and more
                         </Link>
                       </Typography>
-                    </Typography>
+                    </div>
                   </div>
                 </CardContent>
               </Card>
-              <h5 className={classes.bottomRightLabel}>promotion</h5>
+              <Typography variant="h5" className={classes.bottomRightLabel}>promotion</Typography>
             </div>
             <div className={classes.stepper}>
               <li className={classes.sideBarItem}>
-                <label className ={clsx(classes.sliderLabel,toggleSelect? classes.chageColor : '')} style={{marginRight:6}} onClick={onChangeView}> </label>
+                <label className ={clsx(classes.sliderLabel,toggleSelect? classes.chageColor : "")} style={{ marginRight:6 }} onClick={onChangeView}> </label>
               </li>
               <li className={classes.sideBarItem}>
-                <label className ={clsx(classes.sliderLabel,toggleSelect? '' : classes.chageColor)} onClick={onChangeView}> </label>
+                <label className ={clsx(classes.sliderLabel,toggleSelect? "" : classes.chageColor)} onClick={onChangeView}> </label>
               </li>
             </div>
           </div>
